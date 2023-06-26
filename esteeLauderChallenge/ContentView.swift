bd638a2df68807d83629fea64f73ce6c16ee5807
kywee777 <nguyenm.kylie@gmail.com>
2023-06-26 14:01:21 -0700

    Initial Commit

---
+++ esteeLauderChallenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  esteeLauderChallenge
+//
+//  Created by Kylie Nguyen on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ esteeLauderChallenge/esteeLauderChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  esteeLauderChallengeApp.swift
+//  esteeLauderChallenge
+//
+//  Created by Kylie Nguyen on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct esteeLauderChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
